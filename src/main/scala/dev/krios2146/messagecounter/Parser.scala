09fafc19a06813c022b31ed6bcaa760645ddf99e
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-19 04:16:38 +0700

    feat: update Parser with logic to get top users by messages

--- src/main/scala/dev/krios2146/messagecounter/Parser.scala
+++ src/main/scala/dev/krios2146/messagecounter/Parser.scala
@@ -1,3 +1,34 @@
 package dev.krios2146.messagecounter
 
-@main def hello(): Unit = println("Hello World")
+import ujson.Value.Value
+import upickle.default.*
+
+case class TelegramMessage(id: Long, `type`: String, from: Option[String])
+
+@main
+def main(): Unit = {
+  val path = os.pwd / "src" / "main" / "resources" / "result.json"
+
+  val jsonValue: Value = ujson.read(os.read(path))
+  val messageValues: List[Value] = jsonValue("messages").arr.toList
+  val messages = messageValues.map(mapToTelegramData)
+
+  messages
+    .filter(_.`type`.equals("message"))
+    .flatMap(_.from)
+    .groupBy(identity)
+    .view
+    .mapValues(_.size)
+    .toList
+    .sortBy(-_._2)
+    .take(50)
+    .foreach((user, messages) => println(s"$user - $messages"))
+}
+
+def mapToTelegramData(messageValue: Value): TelegramMessage = {
+  TelegramMessage(
+    messageValue("id").num.toLong,
+    messageValue("type").str,
+    messageValue.obj.get("from").flatMap(_.strOpt)
+  )
+}
